8df332b65513036f7eea92db38f0fc976d06be04
Wiktor,Borski W. <i577233@fontysict.nl>
2026-05-29 13:03:47 +0200

    fixxx?

--- init-db.sh
+++ init-db.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 set -eu
 
-until pg_isready -h postgres -p 5432 -U myuser; do
+until pg_isready -h postgres -p 5432 -U myuser -d mydatabase; do
   sleep 1
 done
 
 exists=$(psql -h postgres -p 5432 -U myuser -d mydatabase -tAc "select to_regclass('public.mock_data') is not null")
 if [ "$exists" = "f" ]; then
-  psql -h postgres -p 5432 -U myuser -d mydatabase -f /init.sql
+  psql -h postgres -p 5432 -U myuser -d mydatabase -f /docker-entrypoint-initdb.d/init.sql
 fi
