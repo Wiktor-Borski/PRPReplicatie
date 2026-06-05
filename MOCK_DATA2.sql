15d7b6ad7076f3cc2bc787136cbf2412a0264e5f
Wiktor,Borski W. <i577233@fontysict.nl>
2026-06-05 10:56:21 +0200

    Nieuwe mock data

---
+++ MOCK_DATA2.sql
@@ -0,0 +1,15 @@
+create table MOCK_DATA (
+	id INT,
+	first_name VARCHAR(50),
+	last_name VARCHAR(50)
+);
+insert into MOCK_DATA (id, first_name, last_name) values (1, 'Amble', 'Arboin');
+insert into MOCK_DATA (id, first_name, last_name) values (2, 'Suzann', 'Morillas');
+insert into MOCK_DATA (id, first_name, last_name) values (3, 'Shelia', 'Baldazzi');
+insert into MOCK_DATA (id, first_name, last_name) values (4, 'Billy', 'O''Loughane');
+insert into MOCK_DATA (id, first_name, last_name) values (5, 'Clay', 'Bengtson');
+insert into MOCK_DATA (id, first_name, last_name) values (6, 'Joli', 'Kleinstern');
+insert into MOCK_DATA (id, first_name, last_name) values (7, 'Francisca', 'Brocket');
+insert into MOCK_DATA (id, first_name, last_name) values (8, 'Lucinda', 'Dun');
+insert into MOCK_DATA (id, first_name, last_name) values (9, 'Katalin', 'Stoite');
+insert into MOCK_DATA (id, first_name, last_name) values (10, 'Scott', 'Anscott');
