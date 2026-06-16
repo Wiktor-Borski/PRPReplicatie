dbac6975a9841a782e036f97c85e5b61651a3725
Wiktor,Borski W. <i577233@fontysict.nl>
2026-06-16 09:21:49 +0200

    setup fix

--- setup.reciever.sh
+++ setup.reciever.sh
@@ -6,6 +6,8 @@ rm -rf init_db.sql
 rm -rf init.sql
 rm -rf nginx.conf
 rm -rf setup.sender.sh
+rm -rf watchdog.py
+rm -rf watchdog.DOCKERFILE
 rm -rf docker-compose.yaml
 mv docker-compose.receiver.yaml docker-compose.yaml
 echo "Receiver files cleaned up successfully."
